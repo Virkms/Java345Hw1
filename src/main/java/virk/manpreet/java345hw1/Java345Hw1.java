f0b28693c72f7a613846242e055946145d4477c0
@Virkms <>
2023-01-25 17:00:51 -0500

    Java345Hw1

---
+++ src/main/java/virk/manpreet/java345hw1/Java345Hw1.java
@@ -0,0 +1,12 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package virk.manpreet.java345hw1;
+
+public class Java345Hw1 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
